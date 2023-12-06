10c0c107557129bf3539273ac5d3523b02e5d114
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2023-12-06 12:09:01 -0300

    Update params_model.sql

--- Scripts úteis de exemplo/params_model.sql
+++ Scripts úteis de exemplo/params_model.sql
@@ -1,4 +1,5 @@
 -- A regra é simples: não concatenar inputs do usuário em comandos no seu servidor. Independente de ser uma instrução SQL ou um comando que será executado na máquina.
+-- Usando os parâmetros abaixo, os valores não são inseridos de forma a serem concatenados como inputs, e sim como textos literais.
 
 sql = "INSERT INTO avaliacoes (avaliacao, descricao, url_arquivo, id_usuario, id_produto)" + "VALUES (?,?,?,?,?)"
 params = (avaliacao, descricao, url_arquivo, id_usuario, id_produto)
